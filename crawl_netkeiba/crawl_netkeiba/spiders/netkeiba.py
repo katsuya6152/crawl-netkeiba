037f0353dc7929c458a2a237de0ebe43939e5684
katsuya6152 <katsuya.6152@gmail.com>
2023-01-09 17:25:04 +0900

    conditional search

--- crawl_netkeiba/crawl_netkeiba/spiders/netkeiba.py
+++ crawl_netkeiba/crawl_netkeiba/spiders/netkeiba.py
@@ -2,6 +2,7 @@ import scrapy
 from scrapy_selenium import SeleniumRequest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 
 
@@ -21,8 +22,30 @@ class NetkeibaSpider(scrapy.Spider):
         check_track.click()
         sleep(1)
         check_date = driver.find_element(By.XPATH, '//select[@name="start_year"]')
-        select = Select(check_date)
-        select.select_by_value('2017')
+        select_2017 = Select(check_date)
+        select_2017.select_by_value('2017')
+        sleep(1)
+        for i in range(5, 10):
+            check_place = driver.find_element(By.XPATH, f'//input[@id="check_Jyo_0{i}"]')
+            check_place.click()
+        sleep(1)
+        X = driver.find_element(By.XPATH, '//img[@id="gn_interstitial_close_icon"]')
+        X.click()
+        sleep(1)
+        for i in range(1600, 2700, 100):
+            check_distance = driver.find_element(By.XPATH, f'//input[@id="check_kyori_{i}"]')
+            check_distance.click()
+        check_distance_3000 = driver.find_element(By.XPATH, '//input[@id="check_kyori_3000"]')
+        check_distance_3000.click()
+        check_distance_3200 = driver.find_element(By.XPATH, '//input[@id="check_kyori_3200"]')
+        check_distance_3200.click()
+        sleep(1)
+        check_display = driver.find_element(By.XPATH, '//select[@name="list"]')
+        select_100 = Select(check_display)
+        select_100.select_by_value('100')
+        sleep(1)
+        search = driver.find_element(By.XPATH, '//input[@value="検索"]')
+        search.click()
         sleep(1)
 
         driver.save_screenshot('test.png')
