a0d343ba7b27bcb729e21fdc125d4af217d01c6b
katsuya6152 <katsuya.6152@gmail.com>
2023-04-13 10:28:40 +0900

    add planetscale

--- crawl_netkeiba/crawl_netkeiba/config.py
+++ crawl_netkeiba/crawl_netkeiba/config.py
@@ -3,7 +3,13 @@ load_dotenv(dotenv_path='../mysql/.env')
 
 import os
 
-DB_USER = os.getenv('DB_USER')
-PASSWORD = os.getenv('DB_PASS')
+# If you are in a local environment, use this one
+# DB_USER = os.getenv('DB_USER')
+# PASSWORD = os.getenv('DB_PASS')
+# HOST = os.getenv('LOCAL_HOST')
+# DATABASE = os.getenv('DB_NAME')
+
+DB_USER = os.getenv('USER')
+PASSWORD = os.getenv('PASSWORD')
 HOST = os.getenv('HOST')
-DATABASE = os.getenv('DB_NAME')
+DATABASE = os.getenv('DATABASE')
